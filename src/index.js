89270c1cb642976a75b8bd331d465b1336ea2164
Emmett He <emmetthe26@gmail.com>
2021-10-29 21:12:59 -0700

    finish draw and animate methods for background and player

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import Player from "./scripts/player"
+
 
 document.addEventListener("DOMContentLoaded", function () {
-  const player = new Image();
-  player.src = "../imgs/standing.png"
   const canvasEl = document.getElementById('canvas');
   const ctx = canvasEl.getContext("2d");
+
+  canvasEl.width = 600;
+  canvasEl.height = 800;
+  const keys = [];
+
+  const player = {
+    x: 300, // player start pos x
+    y: 400,  // player start pos y
+    width: 83, // width in px divide by col
+    height: 81,
+    frameX: 0,
+    frameY: 0,
+    speed: 5,
+    moving: false,
+    attacking: false,
+    spriteFrame: 0
+  }
+  const playerImg = new Image();
+  playerImg.src = "./imgs/standing.png";
+
+  const background = new Image();
+  background.src = "./imgs/background.jpg";
+
+  function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH) 
+  }
+ 
+  function animate() {
+    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    ctx.drawImage(background, 0, 0, canvasEl.width, canvasEl.height);
+    drawSprite(playerImg, 0, 0, playerImg.width, playerImg.height, player.x, player.y, playerImg.width, playerImg.height);
+    requestAnimationFrame(animate);
+  }
+  animate();
+
+  window.addEventListener("keydown", keyPress);
+  window.addEventListener("keyup", keyPress);
+
 });
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,30 +1,27 @@
 class Player {
-  constructor () {
-    this.speed = 100;
-    this.x = 300; // player start pos x
-    this.y = 400;  // player start pos y
-    this.attacking = false;
-    this.frameX = 0;
-    this.frameY = 0;
-    this.moving = false;
-  }
-  drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH) 
-  }
-
-
-
-  // document.addEventListener("keydown", (e) => {
-  //   let dir = directionKeys[e]
-    
-  // })
-
-  // movePlayer(pos) {
-  //   if () {
-  //     this.pos[0] += ;
-  //   }
+  // constructor () {
+  //   this.speed = 9;
+  //   this.x = 300; // player start pos x
+  //   this.y = 400;  // player start pos y
+  //   this.frameX = 0;
+  //   this.frameY = 0;
+  //   this.moving = false;
+  //   this.attacking = false;
   // }
+  // drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  //   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH) 
+  // }
+
+
+  // // document.addEventListener("keydown", (e) => {
+  // //   let dir = directionKeys[e]
+    
+  // // })
+
+  // // movePlayer(pos) {
+  // //   if () {
+  // //     this.pos[0] += ;
+  // //   }
+  // // }
   
 }
-
-export default Player;
