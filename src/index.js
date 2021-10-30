3f5ca86ebdc12e35a25e0a9ba8714bb5567c3f0e
Emmett He <emmetthe26@gmail.com>
2021-10-30 00:22:35 -0700

    refactor playermove into player class and updated animate method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import Player from "./scripts/player"
 
 document.addEventListener("DOMContentLoaded", function () {
   const canvasEl = document.getElementById('canvas');
@@ -6,70 +6,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
   canvasEl.width = 600;
   canvasEl.height = 800;
-  const keys = [];
 
-  const player = {
-    x: 300, // player start pos x
-    y: 400,  // player start pos y
-    width: 83, // width in px divide by col
-    height: 81,
-    frameX: 0,
-    frameY: 0,
-    speed: 5,
-    moving: false,
-    attacking: false,
-    // spriteFrame: 0
-  }
-  const playerImg = new Image();
-  playerImg.src = "./imgs/alert_0.png";
+  const player = new Player(300, 400, 83, 81, 0, 0, 5, false, false, "./imgs/walk0.png")
 
   const background = new Image();
   background.src = "./imgs/background.jpg";
+  
+  // image, s = source location starting from top left down right, sW(player width), sH(player height), d = destination of where to draw image
 
-  function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH) 
-  }
- 
   function animate() {
     ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
     ctx.drawImage(background, 0, 0, canvasEl.width, canvasEl.height);
-    drawSprite(playerImg, 0, 0, playerImg.width, playerImg.height, player.x, player.y, playerImg.width, playerImg.height);
-    movePlayer();
+    ctx.drawImage(player.playerImg, 0, 0, player.width, player.height, player.x, player.y, player.width, player.height);
+    player.movePlayer();
     // playerWalkAnimation();
     requestAnimationFrame(animate);
   }
   animate();
 
   window.addEventListener("keydown", function(e) {
-    keys[e.code] = true;
-    player.moving = true;
-    console.log(keys);
+    console.log(e.code)
+    player.addMove(e.code);
   });
   window.addEventListener("keyup", function(e) {
-    delete keys[e.code];
-    player.moving = false;
+    player.deleteMove(e.code);
   });
 
-  function movePlayer() {
-    if (keys["ArrowUp"] && player.y > 50) {
-      player.y -= player.speed;
-    }
-    if (keys["ArrowDown"] && player.y < 650) {
-      player.y += player.speed;
-    }
-    if (keys["ArrowLeft"] && player.x > 15) {
-      player.x -= player.speed;
-    }
-    if (keys["ArrowRight"] && player.x < 500) {
-      player.x += player.speed;
-    }
-  }
-
   // function playerWalkAnimation() {
-  //   if(player.frameX < 3 && player.moving) {
-  //     player.frameX++;
-  //   } else {
-  //     player.frameX = 0;
+  //   // if(player.frameX < 3 && player.moving) {
+  //   //   player.frameX++;
+  //   // } else {
+  //   //   player.frameX = 0;
+  //   // }
+  //   for(let i = 0; i < 5; i++) {
+      
   //   }
   // }
 
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,18 +1,43 @@
-// export default Player;
-// class Player {
-//   constructor () {
-//     this.x = 300; // player start pos x
-//     this.y = 400;  // player start pos y
-//     this.width = 83; // width in px divide by col
-//     this.height = 81;
-//     this.frameX = 0;
-//     this.frameY = 0;
-//     this.speed = 5;
-//     this.moving = false;
-//     this.attacking = false;
-//     this.spriteFrame = 0
-//     this.playerImg = new Image();
-//     this.playerImg.src = "./imgs/standing.png";
-//   }
 
-// }
+export default class Player {
+  constructor (x, y, width, height, frameX, frameY, speed, moving, attacking, src) {
+    this.x = x; // player start pos x
+    this.y = y;  // player start pos y
+    this.width = width; // width in px divide by col
+    this.height = height;
+    this.frameX = frameX;
+    this.frameY = frameY;
+    this.speed = speed;
+    this.moving = moving;
+    this.attacking = attacking;
+    this.playerImg = new Image();
+    this.playerImg.src = src;
+    this.keys = {};
+  }
+
+  addMove (move) {
+    this.keys[move] = true;
+    this.moving = true;
+  }
+
+  deleteMove (move) {
+    delete this.keys[move]
+    this.moving = false;
+  }
+  
+  movePlayer() {
+    if (this.keys["ArrowUp"] && this.y > 50) {
+      this.y -= this.speed;
+    }
+    if (this.keys["ArrowDown"] && this.y < 650) {
+      this.y += this.speed;
+    }
+    if (this.keys["ArrowLeft"] && this.x > 15) {
+      this.x -= this.speed;
+    }
+    if (this.keys["ArrowRight"] && this.x < 500) {
+      this.x += this.speed;
+    }
+  }
+
+}
